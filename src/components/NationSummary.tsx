bbee3128ddc8bf1a3dcb57609f116a10f20959dd
Raphaelj <raphaeljames897@gmail.com>
2024-11-24 21:15:28 +0100

    Update Overview info

--- src/components/NationSummary.tsx
+++ src/components/NationSummary.tsx
@@ -14,21 +14,6 @@ function NationSummary() {
 				and identify areas for improvement. Click on markers to reveal detailed facility
 				information, aiding informed planning and policy-making.
 			</Text>
-			<Text fontSize="sm" marginTop={2} color="gray.focusRing" whiteSpace="wrap">
-				Due to the extensive number of data points, the map works best at closer zoom
-				levels. Explore our{' '}
-				<Link
-					variant="underline"
-					href="https://nigerian-healthcare-map.netlify.app/"
-					target="blank"
-					colorPalette="green"
-					outline="none"
-					display="inline"
-				>
-					innovative solution
-				</Link>{' '}
-				using the green button.
-			</Text>
 			<Box marginTop={6}>
 				<SummaryStats />
 			</Box>
